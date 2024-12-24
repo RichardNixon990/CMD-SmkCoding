c9eb9e825c35bbfd6c51011f40255fa202e61201
RichardNixon990 <richardnixon990@gmail.com>
2024-12-24 13:43:11 +0700

    Create halaman Login & Register

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtikelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\AuthController;
 use App\Models\Post;
 
 Route::get('/', function () {
@@ -20,5 +21,10 @@ Route::post('store', [ArtikelController::class, 'store'])->name('store');
 Route::group(['prefix' => 'kategori', 'as' => 'kategori.'], function () {
     Route::get('add', [KategoriController::class, 'index'])->name('add');
 Route::post('store', [KategoriController::class, 'store'])->name('store');
+});
+
+Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::get('register', [AuthController::class, 'register'])->name('register');
 });
 
